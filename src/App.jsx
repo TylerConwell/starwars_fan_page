ca5055ac40f060bb93bcc69a59bca93b8e8d13a8
Tyler Conwell <wgc14@txstate.edu>
2026-01-19 14:26:04 -0600

    added the vader background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import background from './assets/darth_vader_kneeling_wallpaper.jpg'
 import './App.css'
 
 function App() {
@@ -7,8 +7,18 @@ function App() {
 
   return (
     <>
-      <h1>Star Wars Fan Page</h1>
-      <p>Welcome to the ultimate Star Wars fan page!</p>
+      <div style ={{
+        backgroundImage: `url(${background})`,
+        height: '100vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+
+
+        {/* <h1>Star Wars Fan Page</h1>
+        <p>Welcome to the ultimate Star Wars fan page!</p> */}
+      </div>
+
     </>
   )
 }
