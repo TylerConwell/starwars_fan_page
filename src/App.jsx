c654093a879eca9861efde1f36977719889125b6
Tyler Conwell <wgc14@txstate.edu>
2026-01-19 11:53:47 -0600

    adding all the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Star Wars Fan Page</h1>
+      <p>Welcome to the ultimate Star Wars fan page!</p>
+    </>
+  )
+}
+
+export default App
