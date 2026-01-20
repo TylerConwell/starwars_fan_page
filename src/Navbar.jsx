a24186392b3cb3bb70cd59da55f84b10edc33362
Tyler Conwell <wgc14@txstate.edu>
2026-01-20 10:00:21 -0600

    combat navbar changed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         <ul>
           <li><a href="#home" onClick={() => setIsOpen(false)}>Home</a></li>
           <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
-          <li><a href="#characters" onClick={() => setIsOpen(false)}>Characters</a></li>
+          <li><a href="#combat" onClick={() => setIsOpen(false)}>Combat</a></li>
           <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
         </ul>
       </nav>
